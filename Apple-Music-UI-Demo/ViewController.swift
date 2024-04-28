63bee8950fa476bc2b55259ac7a5f7b6c1384bf3
sebjvidal <seb.vidal@btinternet.com>
2024-04-28 09:22:47 +0100

    Initial Commit

---
+++ Apple-Music-UI-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple-Music-UI-Demo
+//
+//  Created by Seb Vidal on 28/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
